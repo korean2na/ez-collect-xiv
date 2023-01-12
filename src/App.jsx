32b408ace60ac313f236c481f3353061ccf33484
John Han <sirjohn146@gmail.com>
2023-01-12 16:25:40 -0600

    Added alerts for login and logout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { AuthContext } from './contexts/AuthProvider';
 import Home from './views/Home';
@@ -10,6 +10,28 @@ import CharProfile from './views/CharProfile';
 export default function App() {
   const { user, googleLogin, logout } = useContext(AuthContext)
   
+  useEffect(() => {
+    const alertBar = document.getElementById('liveAlertBar')
+
+    const alert = (message, type) => {
+      const wrapper = document.createElement('div')
+      wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible fade show" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+      ].join('')
+  
+      alertBar.append(wrapper)
+    }
+
+    if (user.loggedIn == true) {
+      alert(`Successfully logged in. Welcome back, ${user.displayName}!`, 'success')
+    } else if (user.loggedIn == false) {
+      alert('Successfully logged out.', 'secondary')
+    }
+  }, [user])
+
   return (
     <div className="App" id="app">
       <BrowserRouter>
@@ -25,11 +47,11 @@ export default function App() {
                 <>
                 <li className="nav-item text-light"><strong>Logged in as:</strong></li>
                 <li className="nav-item text-light"><Link to="" id="current-user" className="nav-link active"><strong>{ user.email }</strong></Link></li>
-                <li className="nav-item nav-link active mx-3"><button onClick={logout}  className="btn btn-danger"><strong>Logout</strong></button></li>
+                <li className="nav-item nav-link active mx-3"><button id="logoutBtn" onClick={logout}  className="btn btn-danger"><strong>Logout</strong></button></li>
                 </> :
                 <>
                 <li className="nav-item text-light"><strong>(Currently not logged in.)</strong></li>
-                <li className="nav-item nav-link active"><button onClick={googleLogin} className="btn btn-warning"><strong>Login</strong></button></li>
+                <li className="nav-item nav-link active"><button id="navLoginBtn" onClick={googleLogin} className="btn btn-warning"><strong>Login</strong></button></li>
                 </>
               }
               <li className="text-white text-end" id="titlehead">
@@ -40,6 +62,8 @@ export default function App() {
           </div>
         </nav>
         <hr />
+
+        <div id="liveAlertBar"></div>
 
         <Routes>
           <Route path="/" element={
