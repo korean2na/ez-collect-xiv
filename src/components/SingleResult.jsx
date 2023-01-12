299be8206eacf8bc4aa06251107bb3f36fcfc4b7
John Han <sirjohn146@gmail.com>
2023-01-11 22:27:34 -0600

    Added stats to profile page.

--- src/components/SingleResult.jsx
+++ src/components/SingleResult.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { useContext } from "react"
 import { DataContext } from "../contexts/DataProvider"
 
@@ -49,6 +50,13 @@ export default function SingleResult(props) {
     return (
         <div className="row justify-content-center">
             <div className="card col-6 text-center py-4 mb-5 shadow-lg rounded">
+                {
+                    (status === 'SELECTED') ?
+                    <>
+                        <h5 className="text-end text-black text-opacity-50 mx-3">Currently Selected Character</h5>
+                    </> :
+                    <></>
+                }
                 <hr className="mx-2 mt-0"/>
                 <div className="row align-items-center">
                     <div className="col-8 text-start">
@@ -66,7 +74,11 @@ export default function SingleResult(props) {
                 {
                     (status === 'SELECTED') ?
                         <>
-                            <h5 className="text-muted">Currently Selected Character</h5>
+                            <div className="row justify-content-center">
+                                <div className="col-10">
+                                    <Link to={ '/profile' } className="col-12 btn btn-success">View Profile</Link>
+                                </div>
+                            </div>
                         </> :
                     (status === 'ADDED') ?
                         <>
--- src/views/CharProfile.jsx
+++ src/views/CharProfile.jsx
@@ -4,6 +4,11 @@ import { DataContext } from "../contexts/DataProvider";
 export default function CharProfile() {
     const { char, charInfo } = useContext(DataContext)
 
+    function percent(count, total) {
+        const result = count/total * 100
+        return result.toFixed(1)
+    }
+
     return (
         <div id="CharProfile">
             <div className="row justify-content-center">
@@ -22,26 +27,71 @@ export default function CharProfile() {
                         </div>
                     </div>
                     <hr className="mx-2 pb-2" />
-                    <div className="row justify-content-evenly">
+                    <div className="row justify-content-center">
                         <div className="col-4 px-0">
-                            <div id="portrait-box" className='ms-2'>
-                                <img id="portrait" src={ charInfo.portrait } alt='character portrait'/>
+                            <div className="row mb-3">
+                                <div id="portrait-box" className='ms-4 px-0'>
+                                    <a href={ charInfo.portrait } target="_blank" rel="noopener noreferrer"><img id="portrait" src={ charInfo.portrait } alt='character portrait'/></a>
+                                </div>
+                            </div>
+                            <div className="row justify-content-center">
+                                {
+                                    (charInfo.achievements.public == true) ?
+                                    <>
+                                        <div className="col-10 card py-3 me-4 shadow rounded">
+                                            <h4 className="mb-0">Achievements</h4>
+                                            <hr className="mx-2" />
+                                            <p>{ charInfo.achievements.count } of { charInfo.achievements.total } completed &nbsp; ({ percent(charInfo.achievements.count,charInfo.achievements.total) }%)</p>
+                                            <p>{ charInfo.achievements.points } of { charInfo.achievements.points_total } points earned &nbsp; ({ percent(charInfo.achievements.points,charInfo.achievements.points_total) }%)</p>
+                                            <p>{ charInfo.server } Rank: #{ charInfo.rankings.achievements.server }</p>
+                                            <p>{ charInfo.data_center } Rank: #{ charInfo.rankings.achievements.data_center }</p>
+                                            <p>Global Rank: #{ charInfo.rankings.achievements.global }</p>
+                                        </div>
+                                    </> :
+                                    <></>
+                                }
                             </div>
                         </div>
-                        <div className="col-7 me-3">
-                            <div className="row card py-3 shadow rounded">
-                                <div className="col">
-                                    <h4 className="mb-0">Achievements</h4>
-                                </div>
-                            </div>
-                            <div className="row card py-3 shadow rounded">
+                        <div className="col-6 ps-3 me-3">
+                            <div className="row card py-3 mb-4 shadow rounded">
                                 <div className="col">
                                     <h4 className="mb-0">Mounts</h4>
+                                    <hr className="mx-2" />
+                                    <p>{ charInfo.mounts.count } of { charInfo.mounts.total } collected &nbsp; ({ percent(charInfo.mounts.count,charInfo.mounts.total) }%)</p>
+                                    {
+                                        (charInfo.rankings.mounts.server != null) ?
+                                        <>
+                                            <p>{ charInfo.server } Rank: #{ charInfo.rankings.mounts.server }</p>
+                                            <p>{ charInfo.data_center } Rank: #{ charInfo.rankings.mounts.data_center }</p>
+                                            <p>Global Rank: #{ charInfo.rankings.mounts.global }</p>
+                                        </> :
+                                        <></>
+                                    }
+                                </div>
+                            </div>
+                            <div className="row card py-3 mb-4 shadow rounded">
+                                <div className="col">
+                                    <h4 className="mb-0">Minions</h4>
+                                    <hr className="mx-2" />
+                                    <p>{ charInfo.minions.count } of { charInfo.minions.total } collected &nbsp; ({ percent(charInfo.minions.count,charInfo.minions.total) }%)</p>
+                                    {
+                                        (charInfo.rankings.minions.server != null) ?
+                                        <>
+                                            <p>{ charInfo.server } Rank: #{ charInfo.rankings.minions.server }</p>
+                                            <p>{ charInfo.data_center } Rank: #{ charInfo.rankings.minions.data_center }</p>
+                                            <p>Global Rank: #{ charInfo.rankings.minions.global }</p>
+                                        </> :
+                                        <></>
+                                    }
                                 </div>
                             </div>
                             <div className="row card py-3 shadow rounded">
                                 <div className="col">
-                                    <h4 className="mb-0">Minions</h4>
+                                    <h4 className="mb-0">Relics</h4>
+                                    <hr className="mx-2" />
+                                    <p>{ charInfo.relics.weapons.count } of { charInfo.relics.weapons.total } relic weapons collected &nbsp; ({ percent(charInfo.relics.weapons.count,charInfo.relics.weapons.total) }%)</p>
+                                    <p>{ charInfo.relics.armor.count } of { charInfo.relics.armor.total } relic armor pieces collected &nbsp; ({ percent(charInfo.relics.armor.count,charInfo.relics.armor.total) }%)</p>
+                                    <p>{ charInfo.relics.tools.count } of { charInfo.relics.tools.total } relic tools collected &nbsp; ({ percent(charInfo.relics.tools.count,charInfo.relics.tools.total) }%)</p>
                                 </div>
                             </div>
                         </div>
